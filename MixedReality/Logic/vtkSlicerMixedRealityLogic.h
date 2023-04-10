eacc179702ea97866f07a637ae27dd270f1c8667
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-04-09 21:39:19 -0400

    DOC: Update license headers to be consistent and reference Apache 2.0

--- MixedReality/Logic/vtkSlicerMixedRealityLogic.h
+++ MixedReality/Logic/vtkSlicerMixedRealityLogic.h
@@ -1,11 +1,12 @@
 /*==============================================================================
 
-  Program: 3D Slicer
+  Copyright (c) Kitware Inc.
 
-  Portions (c) Copyright Brigham and Women's Hospital (BWH) All Rights Reserved.
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-  See COPYRIGHT.txt
-  or http://www.slicer.org/copyright/copyright.txt for details.
+    http://www.apache.org/licenses/LICENSE-2.0
 
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
