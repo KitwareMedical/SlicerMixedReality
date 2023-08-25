307923f0b38e50959a2892363a0c474c7aeceeaf
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-08-25 19:57:39 -0400

    COMP: Update VTKExternalModule
    List of changes:
    
    $ git shortlog 10d08094..b03e854a4 --no-merges
    Jean-Christophe Fillion-Robin (3):
          Add SuperBuild support
          Remove redundant BUILD_TESTING option
          COMP: Support configuring install directories

--- FetchVTKExternalModule.cmake
+++ FetchVTKExternalModule.cmake
@@ -6,7 +6,7 @@ set(EP_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj})
 FetchContent_Populate(${proj}
   SOURCE_DIR     ${EP_SOURCE_DIR}
   GIT_REPOSITORY https://github.com/KitwareMedical/VTKExternalModule
-  GIT_TAG        10d08094ac60bedd8a8cc092c67c8816022126ed
+  GIT_TAG        b03e854a416e28f5329e7d30cc871a7aba2e3943
   QUIET
   )
 message(STATUS "Remote - ${proj} [OK]")
